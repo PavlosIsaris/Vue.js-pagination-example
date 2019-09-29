989e160df1372662504f7987e0354dc5c3ca2a92
Paul Isaris <paulisaris@gmail.com>
2019-09-29 12:42:09 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pagination-component></pagination-component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PaginationComponent from './components/PaginationComponent.vue'
+import 'bootstrap';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PaginationComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "~bootstrap/dist/css/bootstrap.min.css";
 </style>
